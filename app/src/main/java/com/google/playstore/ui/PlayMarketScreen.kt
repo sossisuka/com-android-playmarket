ae5d6b707acc12519c81d783fef6622e36e21230
Eduard <edward.tsky@gmail.com>
2026-03-23 12:10:03 +0300

    update UI and API

--- app/src/main/java/com/google/playstore/ui/PlayMarketScreen.kt
+++ app/src/main/java/com/google/playstore/ui/PlayMarketScreen.kt
@@ -35,6 +35,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -319,6 +320,7 @@ fun PlayMarketScreen() {
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .navigationBarsPadding()
                 .background(Color(0xFFE5E5E5))
         ) {
             if (!loading) {
